404e9fd810e45b0a6e8cdab2c10b43ea20bebabe
asyash26 <asyash26@yandex.ru>
2020-09-24 18:11:52 +0300

    Add files via upload

---
+++ Algorithms and data structures/2 week/2_count_sort.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+vector <int> countSort(vector <int> a) {
+	const int count = 100;
+	int i, j = 0;
+	vector<int> cnt(count, 0);
+	for (i = 0; i < a.size(); i++) {
+		cnt[a[i]]++;
+	}
+	for (i = 0; i < count; i++) {
+		while (cnt[i] > 0) {
+			a[j++] = i;
+			cnt[i]--;
+		}
+	}
+	return a;
+}
+
+int main() {
+	int i, buf;
+	vector <int> a;
+	while(cin.peek() != '\n') {
+		cin >> buf;
+		a.push_back(buf);
+	}
+	a = countSort(a);
+	for (i = 0; i < a.size(); i++) {
+		cout << a[i] << " ";
+	}
+}
+
